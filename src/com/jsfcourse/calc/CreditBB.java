4574265b4d2fadd253c7a5b725793d18c638e70e
Wojciech <szmelczerczykwojciech@gmail.com>
2021-10-12 12:03:59 +0200

    initial commit

---
+++ src/com/jsfcourse/calc/CreditBB.java
@@ -0,0 +1,98 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+//import javax.enterprise.context.SessionScoped;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class CreditBB {
+	private String value;
+	private String years;
+	private String percent;
+	private Integer months;
+	private Double q;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getYears() {
+		return years;
+	}
+
+	public void setYears(String years) {
+		this.years = years;
+	}
+	
+	public String getPercent() {
+		return percent;
+	}
+
+	public void setPercent(String percent) {
+		this.percent = percent;
+	}
+	
+	
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double value = Double.parseDouble(this.value);
+			double years = Double.parseDouble(this.years);
+			double percent = Double.parseDouble(this.percent);
+			
+			months = 12;
+			q = 1 + (percent/months);
+			
+			result = ((value * Math.pow(q,years))*((q-1)) / (Math.pow(q,years)-1));
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+	// Put result in messages on AJAX call
+	public String calc_AJAX() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Wynik: " + result, null));
+		}
+		return null;
+	}
+
+	public String info() {
+		return "info";
+	}
+}
